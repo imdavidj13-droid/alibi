fc1f0bacb97985a7046bac43b78b2988e4ca7177
imdavidj13-droid <imdavidj13@gmail.com>
2026-07-25 14:34:57 +0100

    Update Alibi widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,32 +1,35 @@
 import 'package:alibi/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  testWidgets('Alibi home screen loads', (WidgetTester tester) async {
+  setUp(() {
+    SharedPreferences.setMockInitialValues({
+      'alibi_onboarding_seen': true,
+    });
+  });
+
+  testWidgets('Alibi generator screen loads', (tester) async {
     await tester.pumpWidget(const AlibiApp());
+    await tester.pumpAndSettle();
 
     expect(find.text('ALIBI'), findsOneWidget);
-    expect(find.text('Need a\nway out?'), findsOneWidget);
-    expect(find.text('GENERATE'), findsOneWidget);
+    expect(find.text('Say less.\nGet out clean.'), findsOneWidget);
+    expect(find.text('CREATE ALIBI'), findsOneWidget);
     expect(find.text('Believable'), findsOneWidget);
   });
 
-  testWidgets('User can generate and refresh an excuse', (
-    WidgetTester tester,
-  ) async {
+  testWidgets('User can generate an excuse', (tester) async {
     await tester.pumpWidget(const AlibiApp());
+    await tester.pumpAndSettle();
 
-    await tester.tap(find.text('GENERATE'));
+    await tester.ensureVisible(find.text('CREATE ALIBI'));
+    await tester.tap(find.text('CREATE ALIBI'));
     await tester.pumpAndSettle();
 
     expect(find.text('BELIEVABILITY'), findsOneWidget);
     expect(find.text('FOLLOW-UP RISK'), findsOneWidget);
-    expect(find.text('ANOTHER'), findsOneWidget);
-    expect(find.text('COPY'), findsOneWidget);
-
-    await tester.tap(find.text('ANOTHER'));
-    await tester.pumpAndSettle();
-
-    expect(find.text('ANOTHER'), findsOneWidget);
+    expect(find.text('Another'), findsOneWidget);
+    expect(find.text('Copy'), findsOneWidget);
   });
 }
