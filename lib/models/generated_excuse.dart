93dc621f0442fe4056739a094504b774e5a68b59
imdavidj13-droid <imdavidj13@gmail.com>
2026-07-25 14:02:53 +0100

    Add generated excuse model

---
+++ lib/models/generated_excuse.dart
@@ -0,0 +1,23 @@
+enum FollowUpRisk { low, medium, high }
+
+class GeneratedExcuse {
+  const GeneratedExcuse({
+    required this.text,
+    required this.situation,
+    required this.tone,
+    required this.believability,
+    required this.followUpRisk,
+  });
+
+  final String text;
+  final String situation;
+  final String tone;
+  final int believability;
+  final FollowUpRisk followUpRisk;
+
+  String get followUpRiskLabel => switch (followUpRisk) {
+        FollowUpRisk.low => 'LOW',
+        FollowUpRisk.medium => 'MEDIUM',
+        FollowUpRisk.high => 'HIGH',
+      };
+}
